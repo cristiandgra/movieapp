7a8f34e8ac77ac0a6582848c74a08d33656bc244
Cristiandgr <cristiandgr24@gmail.com>
2023-07-11 17:08:19 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import Navbar from './components/Navbar'
